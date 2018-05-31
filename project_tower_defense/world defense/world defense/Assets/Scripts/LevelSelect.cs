e4de0c3a02ebb5f4549ab6c2bf3600b8744fe9a8
DESKTOP-G9ABVHT\pro10 <pro100kill2@gmail.com>
2018-06-01 02:02:07 +0300

    обнова

--- project_tower_defense/world defense/world defense/Assets/Scripts/LevelSelect.cs
+++ project_tower_defense/world defense/world defense/Assets/Scripts/LevelSelect.cs
@@ -5,8 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelect : MonoBehaviour {
      public GameObject level;
-     public void Select(int levelName)
+     public SceneFader sceneFader;
+    public void Select(int levelName)
     {
-        SceneManager.LoadScene(levelName);
+        //SceneManager.LoadScene(levelName);
+
+        sceneFader.FadeTo(levelName);
     }
 }
